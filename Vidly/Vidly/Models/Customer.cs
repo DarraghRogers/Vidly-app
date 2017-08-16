10c062cf4bbd91534bc6e83896fccfcc67309f50
Darragh <darragh.rogers92@gmail.com>
2017-08-16 13:45:32 +0100

    Code First migration and seeding the database

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;//Use this when using the [Required] data anotation
 
 
 namespace Vidly.Models
@@ -9,6 +10,11 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MemberShipType { get; set; }
+        public byte MemberShipTypeId { get; set; }
     }
 }
