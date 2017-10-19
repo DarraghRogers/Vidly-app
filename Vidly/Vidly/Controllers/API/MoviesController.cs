9bf0518a0c4f024a2a1a8f5411d2717d77a8fcbb
Darragh <darragh.rogers92@gmail.com>
2017-10-19 23:03:12 +0100

    added new rentals page and auto-fill

--- Vidly/Vidly/Controllers/API/MoviesController.cs
+++ Vidly/Vidly/Controllers/API/MoviesController.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Vidly.Dtos;
 using Vidly.Models;
+using Microsoft.Ajax.Utilities;
 
 namespace Vidly.Controllers.API
 {
@@ -20,10 +21,18 @@ namespace Vidly.Controllers.API
             _context = new ApplicationDbContext();
         }
         //GET /api/movies
-        public IHttpActionResult GetMovies()
+        public IHttpActionResult GetMovies(string query = null)
         {
-            var movieDtos =  _context.Movies
-                .Include(m => m.Genre)
+            var moviesQuery = _context.Movies
+                .Include(m => m.Genre);
+                //.Where(m => m.NumberAvailable > 0);
+
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+            }
+
+            var movieDtos = moviesQuery
                 .ToList()
                 .Select(Mapper.Map<Movie, MovieDto>);
 
